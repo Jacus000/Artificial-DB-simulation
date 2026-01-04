c9ff301a85e4e3bbfbdea9b7727321b1a4823d83
Michal <mkasjaniuk1@gmail.com>
2026-01-04 19:10:32 +0100

    removed load_name_surenames

--- data/data_loader.py
+++ data/data_loader.py
@@ -2,28 +2,6 @@ import pandas as pd
 from pathlib import Path
 
 
-# wczytywanie danych z csv
-def load_names_surenames():
-    # Path do pliku
-    base_path = Path(__file__).parent
-    folder_path = base_path / 'raw_data'
-
-    def read_names_csv(file_name):
-        file_path = folder_path / file_name
-        return pd.read_csv(file_path, sep=None, engine='python', encoding='utf-8')
-
-    try:
-        imiona_meskie = read_names_csv('imiona_meskie.csv')
-        imiona_zenskie = read_names_csv('imiona_zenskie.csv')
-        nazwiska_meskie = read_names_csv('nazwiska_meskie.csv')
-        nazwiska_zenskie = read_names_csv('nazwiska_zenskie.csv')
-
-        return imiona_meskie, imiona_zenskie, nazwiska_meskie, nazwiska_zenskie
-
-    except FileNotFoundError as e:
-        print(f"Bład : Nie znaleziono pliku")
-        return None
-
 # data_names nazwy plikow w postaci tablicy stringow
 def load_data(data_names):
     # Path do pliku
--- python/load_into_db/load_func.py
+++ python/load_into_db/load_func.py
@@ -1,11 +1,12 @@
 from python.db.connector import get_engine
 
 "Przelanie danych z Pandasa do bazy danych na serwerze"
-def load_df_to_sql(df, df_name, engine = None):
+# table_name nazwa tabeli w bazie danych
+def load_df_to_sql(df, table_name, engine = None):
     if engine is None:
         engine = get_engine()
     try:
-        df.to_sql(df_name, con=engine, if_exists='append', index=False)
+        df.to_sql(table_name, con=engine, if_exists='append', index=False)
         print("OK")
     except Exception as e:
         print(f"Błąd podczas połączenia: {e}. ")
